612c297c2b030b57d26b205afe187363e90f12d1
Fabian Dellwing <fabian.dellwing@mbconnectline.de>
2022-01-09 12:17:19 +0100

    Create functions.js

---
+++ custom/functions.js
@@ -0,0 +1,17 @@
+function scan(ns, parent, server, list) {
+	const children = ns.scan(server);
+	for (let child of children) {
+		if (parent == child) {
+			continue;
+		}
+		list.push(child);
+
+		scan(ns, server, child, list);
+	}
+}
+
+export function list_servers(ns) {
+	const list = [];
+	scan(ns, '', 'home', list);
+	return list;
+}
