fb97e79d81b6cd0a1ad6b140b33298ca83d42f6c
Fabian Dellwing <fabian.dellwing@mbconnectline.de>
2022-01-09 12:23:59 +0100

    Update basic_hack.js

--- basic_hack.js
+++ basic_hack.js
@@ -9,9 +9,9 @@ export async function main(ns) {
         return;
     }
     while (true) {
-        if (ns.getServerSecurityLevel(hostname) > ns.getServerMinSecurityLevel(hostname)) {
+        if (ns.getServerSecurityLevel(hostname) > ns.getServerMinSecurityLevel(hostname) + 5) {
             await ns.weaken(hostname);
-        } else if (ns.getServerMoneyAvailable(hostname) < ns.getServerMaxMoney(hostname)) {
+        } else if (ns.getServerMoneyAvailable(hostname) < ns.getServerMaxMoney(hostname) * 0.75) {
             await ns.grow(hostname);
         } else {
             await ns.hack(hostname);
